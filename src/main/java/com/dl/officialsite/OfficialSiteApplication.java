90ddae61fc9b5aa70a41beb07a938ee0efdfb941
liberhex <holiberhex@gmail.com>
2023-09-25 22:23:40 +0800

    add spring boot initial

---
+++ src/main/java/com/dl/officialsite/OfficialSiteApplication.java
@@ -0,0 +1,13 @@
+package com.dl.officialsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialSiteApplication.class, args);
+	}
+
+}
