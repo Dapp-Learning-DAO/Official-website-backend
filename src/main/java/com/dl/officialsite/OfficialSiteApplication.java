0604e17281ce669a6b480871bf5fc70ccc6835e7
liberhex <holiberhex@gmail.com>
2023-09-27 18:24:40 +0800

    add mail

--- src/main/java/com/dl/officialsite/OfficialSiteApplication.java
+++ src/main/java/com/dl/officialsite/OfficialSiteApplication.java
@@ -2,6 +2,12 @@ package com.dl.officialsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class OfficialSiteApplication {
@@ -10,4 +16,35 @@ public class OfficialSiteApplication {
 		SpringApplication.run(OfficialSiteApplication.class, args);
 	}
 
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.qq.com");
+		//mailSender.setPort(587);
+		mailSender.setPort(465);
+
+		mailSender.setUsername("411497616@qq.com");
+		mailSender.setPassword("XXXXX");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
+
+
+	@Bean
+	public SimpleMailMessage templateSimpleMessage() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setText(
+				"This is the test email template for your email:\n%s\n");
+		return message;
+	}
+
 }
+
+
---
+++ src/main/java/com/dl/officialsite/mail/EmailService.java
@@ -0,0 +1,34 @@
+package com.dl.officialsite.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class EmailService {
+
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    public SimpleMailMessage template;
+
+    public void sendSimpleMessage(
+            String to, String subject, String text) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("411497616@qq.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+
+
+
+
+}
