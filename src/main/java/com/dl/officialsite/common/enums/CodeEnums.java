9a215a9f44bc0d992b9c0369faf0362e6819ec71
yanyanho <411497616@qq.com>
2023-10-16 22:34:50 +0800

    fix the session time and fix the login return

--- src/main/java/com/dl/officialsite/common/enums/CodeEnums.java
+++ src/main/java/com/dl/officialsite/common/enums/CodeEnums.java
@@ -12,7 +12,9 @@ public enum CodeEnums {
     SUCCESSFUL("200", "成功"),
     FAIL("500", "失败"),
     FAIL_DOWNLOAD_FAIL("1001", "文件下载失败"),
-    FAIL_UPLOAD_FAIL("1002", "文件上传失败");
+    FAIL_UPLOAD_FAIL("1002", "文件上传失败"),
+
+    LOGIN_IN("2001", "please login");
 
     private String code;
 
