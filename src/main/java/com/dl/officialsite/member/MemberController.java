044aa0035865caa2c5e9d2fc074d1a3b51f306cf
yanyanho <411497616@qq.com>
2023-10-18 16:38:35 +0800

    add null check

--- src/main/java/com/dl/officialsite/member/MemberController.java
+++ src/main/java/com/dl/officialsite/member/MemberController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
