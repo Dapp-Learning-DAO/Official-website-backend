0281b74bffcc006ba5e9d685262896e0768a0b1a
liberhe <411497616@qq.com>
2023-10-20 11:17:23 +0800

    add redpacket query

--- src/main/java/com/dl/officialsite/team/TeamController.java
+++ src/main/java/com/dl/officialsite/team/TeamController.java
@@ -5,7 +5,9 @@ public class TeamController {
     //1 add create team
     //2 query   all  team
     //3 query by name like
-    // 4 member join and exit team
-        //4.1 join need wechat or  telegram info  .and  send an email to team admin;
+    // 4 member apply to   join   team and  exit team
+        //4.1 join need wechat or telegram info. and  send an email to team admin;
         //4.2 exit  team , send an email to team admin;
+    // 5 team admin approve
+
 }
