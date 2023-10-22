4d20d44c73d592bf228b3d33331110ac9c29bf7a
chengpengxiang <15503679582@163.com>
2023-10-23 00:06:22 +0800

    fix:TeamController address FileCOntroll fileHahs

--- src/main/java/com/dl/officialsite/team/TeamController.java
+++ src/main/java/com/dl/officialsite/team/TeamController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,7 +36,7 @@ public class TeamController {
      * 新增团队
      */
     @PutMapping
-    BaseResponse add(@RequestBody TeamVO team) {
+    BaseResponse add(@RequestBody TeamVO team, @RequestParam String address) {
         teamService.add(team);
         return BaseResponse.successWithData(team);
     }
@@ -44,7 +45,7 @@ public class TeamController {
      * 加入团队
      */
     @PutMapping("/join")
-    BaseResponse join(@RequestBody TeamMemberJoinVO teamMember) {
+    BaseResponse join(@RequestBody TeamMemberJoinVO teamMember, @RequestParam String address) {
         teamService.join(teamMember);
         return BaseResponse.successWithData(null);
     }
@@ -53,7 +54,7 @@ public class TeamController {
      * 退出团队
      */
     @PostMapping("/exit")
-    BaseResponse exit(@RequestBody TeamMemberJoinVO teamMember) {
+    BaseResponse exit(@RequestBody TeamMemberJoinVO teamMember, @RequestParam String address) {
         teamService.exit(teamMember);
         return BaseResponse.successWithData(null);
     }
@@ -62,7 +63,7 @@ public class TeamController {
      * 审批加入团队
      */
     @PostMapping("/approve")
-    BaseResponse approve(@RequestBody TeamMemberApproveVO teamMemberApproveVO) {
+    BaseResponse approve(@RequestBody TeamMemberApproveVO teamMemberApproveVO, @RequestParam String address) {
         teamService.approve(teamMemberApproveVO);
         return BaseResponse.successWithData(null);
     }
@@ -71,7 +72,7 @@ public class TeamController {
      * 查询所有团队
      */
     @GetMapping("/all")
-    BaseResponse getTeams(@RequestBody TeamQueryVo teamQueryVo) {
+    BaseResponse getTeams(@RequestBody TeamQueryVo teamQueryVo, @RequestParam String address) {
         List<TeamsMembersVo> teamAndMembers = teamService.getTeamAndMembers(teamQueryVo);
         return BaseResponse.successWithData(teamAndMembers);
     }
@@ -80,7 +81,7 @@ public class TeamController {
      * 查询需要审批成员
      */
     @GetMapping("/need/approve")
-    BaseResponse getNeedApproveMembers(Long teamId) {
+    BaseResponse getNeedApproveMembers(Long teamId, @RequestParam String address) {
         List<Member> members = teamService.getNeedApproveMembers(teamId);
         return BaseResponse.successWithData(members);
     }
