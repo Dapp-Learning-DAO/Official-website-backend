02786788d931f3071fa9963e32f8c3af1f1089fd
arc0035 <aoronchu@gmail.com>
2023-10-27 19:11:36 +0800

    .

--- src/main/java/com/dl/officialsite/login/model/SessionUserInfo.java
+++ src/main/java/com/dl/officialsite/login/model/SessionUserInfo.java
@@ -3,8 +3,10 @@ package com.dl.officialsite.login.model;
 import com.dl.officialsite.login.enums.UserRoleEnum;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class SessionUserInfo {
+public class SessionUserInfo implements Serializable {
 
     private String address;
 
