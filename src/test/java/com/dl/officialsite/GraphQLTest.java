b009424275e40af7a0648356aeedb72036fc24eb
liberhe <411497616@qq.com>
2023-11-09 10:55:00 +0800

    add graph query   redpackt

--- src/test/java/com/dl/officialsite/GraphQLTest.java
+++ src/test/java/com/dl/officialsite/GraphQLTest.java
@@ -15,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 
 public class GraphQLTest {
 
@@ -45,6 +47,7 @@ public class GraphQLTest {
     }
 
 
+
     @Test
     public void  test1() {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
@@ -52,30 +55,30 @@ public class GraphQLTest {
             request.setHeader("Content-Type", "application/json");
             // Define your GraphQL query
 
-     String graphQL = "\"{ reserves { name baseLTVasCollateral reserveFactor utilizationRate reserveLiquidationThreshold liquidityRate variableBorrowRate totalDeposits availableLiquidity totalATokenSupply totalCurrentVariableDebt } }\"" ;
+            String graphQL = "\"{ reserves { name baseLTVasCollateral reserveFactor utilizationRate reserveLiquidationThreshold liquidityRate variableBorrowRate totalDeposits availableLiquidity totalATokenSupply totalCurrentVariableDebt } }\"" ;
 
-     //不能有\n 并且要带“”；
-     String graphQL1 = " \"{" +
-             "   reserves {" +
-             "      name" +
-             "     " +
-             "     baseLTVasCollateral" +
-             "      reserveFactor" +
-             "      utilizationRate" +
-             "    reserveLiquidationThreshold" +
-             "      liquidityRate " +
-             "      variableBorrowRate" +
-             "      totalDeposits" +
-             "    " +
-             "      availableLiquidity" +
-             "      totalATokenSupply" +
-             "      totalCurrentVariableDebt" +
-             "    }" +
-             "  } \" ";
+            //不能有\n 并且要带“”；
+            String graphQL1 = " \"{" +
+                    "   reserves {" +
+                    "      name" +
+                    "     " +
+                    "     baseLTVasCollateral" +
+                    "      reserveFactor" +
+                    "      utilizationRate" +
+                    "    reserveLiquidationThreshold" +
+                    "      liquidityRate " +
+                    "      variableBorrowRate" +
+                    "      totalDeposits" +
+                    "    " +
+                    "      availableLiquidity" +
+                    "      totalATokenSupply" +
+                    "      totalCurrentVariableDebt" +
+                    "    }" +
+                    "  } \" ";
 
 
-         String query = "{ \"query\": " +
-                 graphQL1 +
+            String query = "{ \"query\": " +
+                    graphQL1 +
                     " }";
 
 
@@ -111,4 +114,126 @@ public class GraphQLTest {
             e.printStackTrace();
         }
     }
+
+
+    @Test
+    public void  test2() {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost("https://api.studio.thegraph.com/proxy/55957/dapp-learning-redpacket/v0.0.1");
+            request.setHeader("Content-Type", "application/json");
+            // Define your GraphQL query
+
+            String graphQL = "\"{" +
+                    "  claims {" +
+                    "    id" +
+                    "      claimer" +
+                    "    claimedValue" +
+                    "    tokenAddress" +
+                    "    happyRedPacketId" +
+                    "  }" +
+                    "}\"" ;
+
+
+
+            String query = "{ \"query\": " +
+                    graphQL +
+                    " }";
+
+
+            logger.info("%%%%%%  "+  query);
+            request.setEntity(new StringEntity(query));
+
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                String jsonResponse = EntityUtils.toString(entity);
+                // Parse and process the jsonResponse here
+                logger.info("&&&&&&"+ jsonResponse);
+
+
+                JsonObject jsonObject = JsonParser.parseString(jsonResponse).getAsJsonObject();
+
+                // Extract the 'reserves' field
+                JsonObject data = jsonObject.getAsJsonObject("data");
+                JsonArray reservesArray = data.getAsJsonArray("claims");
+
+                System.out.println("Reserve array : " + reservesArray);
+                for (int i = 0; i < reservesArray.size(); i++) {
+                    // Access each element in the array
+                    JsonObject reserveObject = reservesArray.get(i).getAsJsonObject();
+                    String name = reserveObject.get("claimer").getAsString();
+                    System.out.println("claimer address: " + name);
+                }
+                // Use the 'reserveFactor' value as needed
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    public void test3() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost request = new HttpPost("https://api.studio.thegraph.com/proxy/55957/dapp-learning-redpacket/v0.0.1");
+        request.setHeader("Content-Type", "application/json");
+        // Define your GraphQL query
+
+        String graphQL = "\" {" +
+                "  redpackets {" +
+                "    id" +
+                "     claimers {" +
+                "      claimer" +
+                "    }" +
+                "  }" +
+                "}\"" ;
+
+
+        String query = "{ \"query\": " +
+                graphQL +
+                " }";
+
+
+        logger.info("%%%%%%  "+  query);
+        request.setEntity(new StringEntity(query));
+
+        HttpResponse response = httpClient.execute(request);
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            String jsonResponse = EntityUtils.toString(entity);
+            // Parse and process the jsonResponse here
+            logger.info("&&&&&&"+ jsonResponse);
+
+
+            JsonObject jsonObject = JsonParser.parseString(jsonResponse).getAsJsonObject();
+
+            // Extract the 'reserves' field
+            JsonObject data = jsonObject.getAsJsonObject("data");
+            JsonArray reservesArray = data.getAsJsonArray("redpackets");
+
+            System.out.println("redpackt array : " + reservesArray);
+            for (int i = 0; i < reservesArray.size(); i++) {
+                // Access each element in the array
+                JsonObject reserveObject = reservesArray.get(i).getAsJsonObject();
+                String id = reserveObject.get("id").getAsString();
+                System.out.println("id: " + id);
+//                String claimed = reserveObject.get("hasRefundedOrAllClaimed").getAsString();
+//                System.out.println("claimed: " + claimed);
+
+                JsonArray claimers = reserveObject.getAsJsonArray("claimers");
+                for(int j =0 ; j <claimers.size(); j++ ) {
+
+                    System.out.println("claimer address: " + claimers.get(j).getAsJsonObject().get("claimer"));
+                }
+            }
+            // Use the 'reserveFactor' value as needed
+
+        }
+
+    }
+
 }
