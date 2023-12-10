c66ee583fdb5e53f1fff3dcbd25c19b2afb22c61
chengpengxiang <15503679582@163.com>
2023-12-10 22:33:53 +0800

    feat:添加招聘模块的update,批量更新,批量添加成员

--- src/main/java/com/dl/officialsite/hiring/HireRepository.java
+++ src/main/java/com/dl/officialsite/hiring/HireRepository.java
@@ -1,6 +1,9 @@
 package com.dl.officialsite.hiring;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @ClassName HireRepository
@@ -9,4 +12,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @Description TODO
  **/
 public interface HireRepository extends JpaRepository<Hiring, Long> {
+
+
+    /**
+     * 根据地址查询所有简历
+     */
+    @Query(value = "select * from hiring where address = :address", nativeQuery = true)
+    List<Hiring> findAllByAddress(@Param("address")String address);
 }
--- src/main/java/com/dl/officialsite/team/Team.java
+++ src/main/java/com/dl/officialsite/team/Team.java
@@ -1,15 +1,10 @@
 package com.dl.officialsite.team;
 
-import com.dl.officialsite.member.Member;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.List;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Data
@@ -20,8 +15,11 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    //team名称
     private String teamName;
+    //团队简介
     private String teamProfile;
+    //管理员
     private String administrator;
 
     // dao admin , core contributor,  builder
