ac5370f82dd6c5658f7c7f61b4b6320347f2aa30
liberhe <411497616@qq.com>
2024-01-14 17:38:42 +0800

    delete the useless code

--- src/main/java/com/dl/officialsite/hiring/application/ApplicationRepository.java
+++ src/main/java/com/dl/officialsite/hiring/application/ApplicationRepository.java
@@ -1,12 +1,7 @@
 package com.dl.officialsite.hiring.application;
 
-import com.dl.officialsite.hiring.Hiring;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 
 public interface ApplicationRepository extends JpaRepository<Application, Long>,
