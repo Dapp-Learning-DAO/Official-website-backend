7a80efa2e274419aae146724070035f3a1388860
liberhe <411497616@qq.com>
2024-01-20 19:14:23 +0800

    add init sql

---
+++ src/main/resources/DB/2.sql
@@ -0,0 +1,19 @@
+-- 原因:createName修改为creatorName
+drop table application;
+create table application
+(
+    id           bigint auto_increment
+        primary key,
+    create_time  datetime     not null,
+    creator_name varchar(255) null,
+    hiring_id    bigint       null,
+    member_id    bigint       null,
+    member_name  varchar(255) null,
+    status       int          null,
+    update_time  datetime     null
+)
+    engine = InnoDB;
+
+delete  from red_packet;
+alter table red_packet MODIFY COLUMN id bigint;
+
