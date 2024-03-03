27886cacf8228980e724eba5366f48aab20b7ffc
chengpengxiang <15503679582@163.com>
2024-03-03 20:07:29 +0800

    fix: update LocalDateTime to Long

--- src/main/java/com/dl/officialsite/bounty/vo/BountyVo.java
+++ src/main/java/com/dl/officialsite/bounty/vo/BountyVo.java
@@ -53,11 +53,9 @@ public class BountyVo {
     //流支付ID
     private String streamId;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private LocalDateTime streamStart;
+    private Long streamStart;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private LocalDateTime streamEnd;
+    private Long streamEnd;
 
     private String streamChainId;
 
