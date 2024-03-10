b0ab446cca7fffd5a6bb8a75e103669acc8731d6
chengpengxiang <15503679582@163.com>
2024-03-10 12:13:52 +0800

    feat: get token apy

--- src/main/java/com/dl/officialsite/aave/AaveTokenAPYService.java
+++ src/main/java/com/dl/officialsite/aave/AaveTokenAPYService.java
@@ -3,14 +3,19 @@ package com.dl.officialsite.aave;
 import static org.web3j.tx.gas.DefaultGasProvider.GAS_LIMIT;
 import static org.web3j.tx.gas.DefaultGasProvider.GAS_PRICE;
 
+import com.dl.officialsite.config.ChainInfo;
+import com.dl.officialsite.config.Web3jAutoConfiguration;
 import com.dl.officialsite.contract.ipool.IPool;
 import com.dl.officialsite.contract.ipooladdressesprovider.IPoolAddressesProvider;
 import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
@@ -35,50 +40,107 @@ public class AaveTokenAPYService extends AbstractTokenAPY {
     BigInteger e8 = new BigInteger("100000000");
 
     BigInteger E23 = new BigInteger("100000000000000000000000");
-    static Map<String, Map<String, String>> chainWithToken = new HashMap<>();
-    static {
-        Map<String, String> optimismTokenMap = new HashMap<>();
-        optimismTokenMap.put("usdc", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174");
-        optimismTokenMap.put("usdt", "0xc2132D05D31c914a87C6611C10748AEb04B58e8F");
-        optimismTokenMap.put("dai", "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063");
-        optimismTokenMap.put("eth", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619");
-        optimismTokenMap.put("btc", "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6");
-        chainWithToken.put("optimism", optimismTokenMap);
-    }
+    static ConcurrentHashMap<ChainInfo, Web3j> chainWithWeb3j = Web3jAutoConfiguration.web3jMap;
 
+    private final TokenAPYInfoRepository tokenAPYInfoRepository;
 
-    private  Web3j web3j;
-
-    private final Credentials credentials;
-
-    private final IPoolAddressesProvider poolAddressesProvider;
-
-    public AaveTokenAPYService(Credentials credentials,
-        IPoolAddressesProvider poolAddressesProvider) {
-//        this.web3j = web3j;
-        this.credentials = credentials;
-        this.poolAddressesProvider = poolAddressesProvider;
+    public AaveTokenAPYService(TokenAPYInfoRepository tokenAPYInfoRepository) {
+        this.tokenAPYInfoRepository = tokenAPYInfoRepository;
     }
 
     @Override
     public List<TokenAPYInfo> queryTokenApy() {
+        return tokenAPYInfoRepository.findAll();
+    }
+
+    @Override
+    public HealthInfo getHealthInfo(ChainInfo chainInfo, String address) {
+        try {
+            AtomicReference<HealthInfo> healthInfo = new AtomicReference<>();
+            chainWithWeb3j.forEach((chain, web3j) -> {
+                if (chain.getId().equals(chainInfo.getId())) {
+                    log.info("chain: {}", chain.getName());
+                    IPoolAddressesProvider addressesProvider = IPoolAddressesProvider.load(
+                        chain.getLendingPoolAddressProviderV3Address(), web3j, getCredentials(), GAS_PROVIDER);
+                    String poolAddress = null;
+                    try {
+                        poolAddress = addressesProvider.getPool().send();
+                    } catch (Exception e) {
+                        log.error("get pool address error: {}, IPoolAddressesProviderAddress{}",
+                            chain.getLendingPoolAddressProviderV3Address(), e.getMessage());
+                        throw new RuntimeException(e);
+                    }
+                    log.info("poolAddress is : {}", poolAddress);
+                    IPool pool = IPool.load(poolAddress, web3j, getCredentials(), GAS_PROVIDER);
+                    try {
+                        Tuple6<BigInteger, BigInteger, BigInteger, BigInteger, BigInteger, BigInteger> info =
+                            pool.getUserAccountData(address).send();
+                        BigInteger totalCollateralBase = info.component1();
+                        BigInteger totalDebtBase = info.component2();
+                        BigInteger ltv = info.component5();
+                        BigInteger healthFactor = info.component6();
+                        healthInfo.set(HealthInfo.builder()
+                            .healthFactor(healthFactor)
+                            .totalBorrows(totalDebtBase.toString())
+                            .totalCollateralETH(totalCollateralBase.toString())
+                            .totalLiquidity(ltv.toString())
+                            .build());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            return healthInfo.get();
+        } catch (Exception e) {
+            log.error("getHealthInfo error: " + e.getMessage());
+            throw new RuntimeException("get health Info error");
+        }
+    }
+
+    public List<ChainInfo> queryChainList() {
+        return new ArrayList<>(Web3jAutoConfiguration.web3jMap.keySet());
+    }
+
+    @Scheduled(cron =  "${jobs.defi.corn: 0 30 * * * * ?}")
+    public void updateTokenAPYInfo()  {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        log.info("update token info task begin --------------------- ");
+        log.info("now date {}", LocalDateTime.now().format(formatter));
+        tokenAPYInfoRepository.deleteAll();
+        List<TokenAPYInfo> tokenAPYInfoList = queryTokenApyOnChain();
+        tokenAPYInfoRepository.saveAll(tokenAPYInfoList);
+    }
+
+    public List<TokenAPYInfo> queryTokenApyOnChain() {
         List<TokenAPYInfo> tokenAPYInfoList = new ArrayList<>();
         try {
-            String poolAddress = poolAddressesProvider.getPool().send();
-            log.info("poolAddress is : {}", poolAddress);
-            IPool pool = IPool.load(poolAddress, web3j, credentials, GAS_PROVIDER);
-            chainWithToken.forEach((chain, tokenMap) -> {
-                tokenMap.forEach((tokenName, tokenAddress) -> {
-                    log.info("chain: {} tokenName: {} tokenAddress: {}", chain, tokenName, tokenAddress);
+            chainWithWeb3j.forEach((chain, web3j) -> {
+                log.info("chain: {}", chain.getName());
+                IPoolAddressesProvider addressesProvider =
+                    IPoolAddressesProvider.load(chain.getLendingPoolAddressProviderV3Address(), web3j,
+                    getCredentials(), GAS_PROVIDER);
+                String poolAddress = null;
+                try {
+                    poolAddress = addressesProvider.getPool().send();
+                } catch (Exception e) {
+                    log.error("get pool address error: {}, IPoolAddressesProviderAddress{}",
+                        chain.getLendingPoolAddressProviderV3Address(), e.getMessage());
+                    throw new RuntimeException(e);
+                }
+                log.info("poolAddress is : {}", poolAddress);
+                IPool pool = IPool.load(poolAddress, web3j, getCredentials(), GAS_PROVIDER);
+                chain.getTokens().forEach(token -> {
+                    log.info("tokenName: {} tokenAddress: {}", token.getName(), token.getAddress());
                     try {
-                        IPool.ReserveData reserveData = pool.getReserveData(tokenAddress).send();
-                        log.info("{} variable deposit interest: {}", tokenName, reserveData.currentVariableBorrowRate.divide(E23).floatValue() / 100);
+                        IPool.ReserveData reserveData = pool.getReserveData(token.getAddress()).send();
+                        log.info("{} variable deposit interest: {}", token.getName(), reserveData.currentVariableBorrowRate.divide(E23).floatValue() / 100);
                         String tokenApy = reserveData.currentLiquidityRate.divide(E23).floatValue() / 100 + "%";
                         TokenAPYInfo tokenAPYInfo = new TokenAPYInfo();
-                        tokenAPYInfo.setTokenName(tokenName);
-                        tokenAPYInfo.setTokenAddress(tokenAddress);
-                        tokenAPYInfo.setChainName(chain);
+                        tokenAPYInfo.setTokenName(token.getName());
+                        tokenAPYInfo.setTokenAddress(token.getAddress());
+                        tokenAPYInfo.setChainName(chain.getName());
                         tokenAPYInfo.setTokenApy(tokenApy);
+                        tokenAPYInfo.setProtocol("Aave");
                         tokenAPYInfoList.add(tokenAPYInfo);
                     } catch (Exception e) {
                         log.error("get reserve data error: {}", e.getMessage());
@@ -87,38 +149,13 @@ public class AaveTokenAPYService extends AbstractTokenAPY {
                 });
             });
         } catch (Exception e) {
-            log.error("get pool address error: {}", e.getMessage());
-            throw new RuntimeException("get token apy error", e);
+            log.error("queryTokenApyOnChain error: {}", e.getMessage());
+            throw new RuntimeException("query token apy error", e);
         }
         return tokenAPYInfoList;
     }
 
-    @Override
-    public HealthInfo getHealthInfo(String address) {
-        try {
-            String poolAddress = poolAddressesProvider.getPool().send();
-            log.info("poolAddress is : " + poolAddress);
-            IPool pool = IPool.load(poolAddress, web3j, credentials, GAS_PROVIDER);
-            Tuple6<BigInteger, BigInteger, BigInteger, BigInteger, BigInteger, BigInteger> info = pool.getUserAccountData(
-                address).send();
-            BigInteger totalCollateralBase = info.component1();
-            BigInteger totalDebtBase = info.component2();
-            BigInteger ltv = info.component5();
-            BigInteger healthFactor = info.component6();
-            HealthInfo healthInfo = HealthInfo.builder()
-                .healthFactor(healthFactor)
-                .totalBorrows(totalDebtBase.toString())
-                .totalCollateralETH(totalCollateralBase.toString())
-                .totalLiquidity(ltv.toString())
-                .build();
-            return healthInfo;
-        } catch (Exception e) {
-            log.error("getHealthInfo error: " + e.getMessage());
-            throw new RuntimeException("get health Info error");
-        }
-    }
-
-    public List<String> queryChainList() {
-        return new ArrayList<>(chainWithToken.keySet());
+    private Credentials getCredentials()   {
+        return Credentials.create("1");
     }
 }
