5739dc55c22d9a9ea1b136febf9b92c1193cec9e
Hongbin.Yuan <harryhongbin.yuan@astrazeneca.com>
2024-03-11 21:50:08 +0100

    add hello controller

---
+++ src/main/java/com/dl/officialsite/login/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.dl.officialsite.login.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
