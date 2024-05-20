c9ebd6b518eddfedb97202b02c26841bcd22f865
Yuan, Harry Hongbin <yuanhongbin9090@gmail.com>
2024-05-21 00:46:34 +0200

    Add NFT claim query API (#250)
    * fix error and add CacheDevController
    
    * add NFT claim query API

---
+++ src/main/java/com/dl/officialsite/nft/service/WarCraftContractService.java
@@ -0,0 +1,65 @@
+package com.dl.officialsite.nft.service;
+
+import com.dl.officialsite.common.base.BaseResponse;
+import com.dl.officialsite.nft.config.ContractConfigService;
+import com.dl.officialsite.nft.constant.ContractNameEnum;
+import com.dl.officialsite.nft.contract.WarCraftContract;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.ClientTransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.StaticGasProvider;
+
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class WarCraftContractService {
+    private static final String ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+    private static final ContractGasProvider ZERO_GAS_PROVIDER = new StaticGasProvider(BigInteger.ZERO, BigInteger.ZERO);
+    public static final long TIMEOUT = 5;
+
+    @Autowired
+    private ContractConfigService contractConfigService;
+
+    public BaseResponse rank(String address, ContractNameEnum contractName, String chainId) {
+        String contractAddress = this.contractConfigService.getContractAddressByName(contractName, chainId);
+        String rpcAddress = this.contractConfigService.getRpcAddressByName(contractName, chainId);
+
+        if (StringUtils.isAnyBlank(contractAddress, rpcAddress)) {
+            log.error("Fetch blank values of contractAddress:[{}] or rpcAddress:[{}] filtered by contractName:[{}] and chainId:[{}]",
+                contractAddress, rpcAddress, contractName.getContractName(), chainId);
+            return BaseResponse.failWithReason("1204", "Fetch rank failed, please try again later.");
+        }
+
+        Web3j web3j = Web3j.build(new HttpService(rpcAddress));
+
+        ClientTransactionManager transactionManager = new ClientTransactionManager(web3j, ZERO_ADDRESS);
+        WarCraftContract contract = WarCraftContract.load(contractAddress, web3j, transactionManager, ZERO_GAS_PROVIDER);
+
+        try {
+            BigInteger tokenId = contract.claimedTokenIdBy(address).sendAsync().get(TIMEOUT, TimeUnit.SECONDS);
+            if (tokenId == null || tokenId.intValue() == 0){
+                log.error("No claim(tokenId) info found for address:[{}].", address);
+                return BaseResponse.failWithReason("1206", "No claim info found.");
+            }
+
+            String rank = contract.tokenURI(tokenId).sendAsync().get(TIMEOUT, TimeUnit.SECONDS);
+            if (StringUtils.isBlank(rank)){
+                log.error("No rank info found for address:[{}] and tokenId:[{}].", address, tokenId.intValue());
+                return BaseResponse.failWithReason("1206", "No rank info found.");
+            }
+            return BaseResponse.successWithData(rank);
+        } catch (Exception e) {
+            log.error("Call contract method:[claimedTokenIdBy] remote error.", e);
+            return BaseResponse.failWithReason("1205", "Fetch rank failed, please try again later.");
+        }
+
+    }
+
+}
